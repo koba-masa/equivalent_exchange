938591b31ab8e43fbb27554d9ad4c196b81a6d73
koba-masa <kobayashi_mshr@koba-masa.com>
2023-06-25 21:21:33 +0900

    feat: パスエイリアスに変更

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-// eslint-disable-next-line import/no-absolute-path
-import viteLogo from '/vite.svg'
-import './App.css'
+import reactLogo from '@/assets/react.svg'
+import viteLogo from '~/vite.svg'
+import '@/App.css'
 
 const App: React.FunctionComponent = () => {
   const [count, setCount] = useState(0)
