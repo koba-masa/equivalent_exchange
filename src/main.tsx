cb3d3669810b91a3d1a0f61f5abc11574892d8f3
koba-masa <kobayashi_mshr@koba-masa.com>
2023-06-25 21:43:32 +0900

    feat: サンプルファイルを削除

--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from '@/App.tsx'
-import '@/index.css'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import { BrowserRouter, Routes } from 'react-router-dom'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-      </Routes>
+      <Routes></Routes>
     </BrowserRouter>
   </React.StrictMode>
 )
