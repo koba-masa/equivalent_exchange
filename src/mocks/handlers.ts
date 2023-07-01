9c3b1a0ff01167f481d796b76011674117da4fa1
koba-masa <kobayashi_mshr@koba-masa.com>
2023-07-01 23:04:36 +0900

    feat: 欲しいもの詳細用のモックを実装

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -1,10 +1,12 @@
 import { rest } from 'msw'
 import Login from '@/mocks/resolvers/mockLogin.ts'
 import MockWants from '@/mocks/resolvers/mockWants.ts'
+import MockWantDetail from '@/mocks/resolvers/mockWantDetail'
 
 const handlers = [
   rest.post('*/v1/login', Login),
-  rest.get('/v1/users/:userId/wants', MockWants)
+  rest.get('/v1/users/:userId/wants', MockWants),
+  rest.get('/v1/users/:userId/wants/:wantId', MockWantDetail)
 ]
 
 export default handlers
